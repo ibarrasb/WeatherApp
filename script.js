a97c08c4bda61eeea41eb6606ddc8e5b58acac9a
Eduardo Ibarra <eddieibarra43@gmail.com>
2020-11-04 20:00:31 -0600

    append working

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-var searchBtn = $("#search-button");
-var searchBar = $("#search-bar");
+var searchBtn = document.querySelector("#search-button");
+var searchBar = document.querySelector("#search-bar");
+var CitiesList = document.querySelector("#cities");
 
+//Creates Array for all cities that are inputted
+var Namecities = [];
 
-searchBtn.on("click", function(){
+//calls cities function
+renderCities();
 
-    // console.log("worked")
+//Creates a new button element once user searches for the city
+function renderCities() {
+     CitiesList.innerHTML = " ";
 
+     //for loop to iterate through the list of cities
+    for(var i =0; i< Namecities.length; i++){
+        var Namecitie = Namecities[i];
 
-var results = searchBar.val();
+        var li = document.createElement("button");
+        li.textContent = Namecitie;
+        li.setAttribute("id", "list-cities")
+        li.setAttribute("class", "btn-cities")
+        CitiesList.appendChild(li);
+    }
+}
 
-console.log(results);
+//Calls function once button is clicked
+searchBtn.addEventListener("click", function(event){
+    event.preventDefault();
 
-searchBar.val('');
+var results = searchBar.value.trim();
+
+//doesnt append anything if search bar is empty
+if(results === ""){
+    return;
+}
+//pushes the city into the array
+Namecities.push(results);
+//clears search bar for new input
+searchBar.value = "";
+
+renderCities();
 })
 
