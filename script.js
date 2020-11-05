7b1a8e73050baa6e177f8f784c36ed82844b7fe4
w_dimuthu <w.dimuthu@directfn.com>
2020-11-05 12:54:05 +0530

    changes done by Dimuthu

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-var searchBtn = document.querySelector("#search-button");
-var searchBar = document.querySelector("#search-bar");
-var CitiesList = document.querySelector("#cities");
-var newButton = document.querySelector("#cities");
+var searchBtn = document.getElementById("search-button");
+var searchBar = document.getElementById("search-bar");
+var CitiesList = document.getElementById("cities");
 
 //Creates Array for all cities that are inputted
 var Namecities = [];
@@ -21,10 +20,31 @@ function renderCities() {
         var Namecitie = Namecities[i];
         var li = document.createElement("button");
         li.textContent = Namecitie;
-        li.setAttribute("id", "list-cities")
+        li.setAttribute("id", "list-cities-"+i)
         li.setAttribute("class", "btn-cities")
         li.setAttribute("data-city", Namecitie)
         CitiesList.appendChild(li);
+    }
+
+    for (let i=0; i<Namecities.length; i++) {
+        (function (i) {
+            let btn = document.getElementById("list-cities-"+i);
+            btn.addEventListener('click', function() { 
+                let city = btn.firstChild.data;
+                console.log(city);
+
+
+                var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=77cb75e1dce4e2e8df315c39afb955ef";
+
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }).then(function(response) {
+                    let results = response;
+                    console.log(results);
+                });
+            });
+        })(i);
     }
 }
 
@@ -32,47 +52,21 @@ function renderCities() {
 searchBtn.addEventListener("click", function(event){
     event.preventDefault();
 
-var results = searchBar.value.trim();
+    var results = searchBar.value.trim();
 
-//doesnt append anything if search bar is empty
-if(results === ""){
-    return;
-}
-//pushes the city into the array
-Namecities.push(results);
-//clears search bar for new input
-searchBar.value = "";
+    //doesnt append anything if search bar is empty
+    if(results === ""){
+        return;
+    }
+    //pushes the city into the array
+    Namecities.push(results);
+    //clears search bar for new input
+    searchBar.value = "";
 
-renderCities();
-})
-
-newButton.addEventListener("click", function(){
-//CANT GET VALUE FROM SELECTED BUTTON
-
-var city = this.getAttribute('#list-cities');
-
-console.log(city);
-
-
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=77cb75e1dce4e2e8df315c39afb955ef";
-
-
-
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   })
-//     .then(function(response) {
-
-//         var results = response;
-//         console.log(results);
-
-
-//     });
-
-
-
+    renderCities();
 });
+
+
 
 
 
